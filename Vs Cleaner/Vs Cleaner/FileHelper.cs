d4631185647f9a21cf90c0071d59eb43b86628b0
Jebb Burditt <jebb.burditt@gmail.com>
2019-04-23 16:15:33 -0700

    first working copy

---
+++ Vs Cleaner/Vs Cleaner/FileHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Vs_Cleaner
+{
+    public static class FileHelper
+    {
+        public static List<string> FindDirectories(string rootDirectoryPath, Regex searchRegex, bool includeSubDirectories = false)
+        {
+            var allDirectoryPaths = GetDirectories(rootDirectoryPath, includeSubDirectories);
+            List<string> matchingDirectoryPaths = new List<string>();
+
+            foreach (var directoryPath in allDirectoryPaths)
+            {
+                var directoryName = new DirectoryInfo(directoryPath).Name;
+
+                if (searchRegex.Match(directoryName).Success)
+                    matchingDirectoryPaths.Add(directoryPath);
+            }
+
+            return matchingDirectoryPaths;
+        }
+
+        public static List<string> GetDirectories(string rootDirectoryPath, bool includeSubDirectories = false)
+        {
+            // Get all subdirectories
+            var directoryPaths = Directory.GetDirectories(rootDirectoryPath).ToList();
+
+            if (!includeSubDirectories || directoryPaths.Count == 0)
+                return directoryPaths;
+
+            // Loop through them to see if they have any other subdirectories
+            for (var i = 0; i < directoryPaths.Count; i++)
+            {
+                var subdirectoryPaths = GetDirectories(directoryPaths[i]);
+                directoryPaths.AddRange(subdirectoryPaths);
+            }
+
+            return directoryPaths;
+        }
+    }
+}
---
+++ Vs Cleaner/Vs Cleaner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Vs_Cleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var applicationRootPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            Console.WriteLine($"Root filepath is '{applicationRootPath}'.");
+
+            var searchRegex = new Regex("(bin$|obj$)");
+            var directoryPaths = FileHelper.FindDirectories(applicationRootPath, searchRegex, true);
+
+            if (directoryPaths == null || directoryPaths.Count == 0)
+                Console.WriteLine("No directories found.");
+            else
+                Console.WriteLine("Found the following directories.");
+
+            foreach (var directoryPath in directoryPaths)
+                Console.WriteLine(directoryPath);
+        }
+    }
+}
